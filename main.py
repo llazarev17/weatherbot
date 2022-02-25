163c81b4da5701bc047d4ba0f4974ff247f08928
llazarev <alaz03@yandex.ru>
2022-02-25 14:51:53 +0300

    feat: setup telebot - add message handler

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+import telebot
+import config
+
+bot = telebot.TeleBot(config.token)
+print(bot.get_me())
+
+
+@bot.message_handler(content_types=["text"])
+def handle_text(massege):
+    bot.send_message(massege.chat.id, "Hi")
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    bot.polling()
